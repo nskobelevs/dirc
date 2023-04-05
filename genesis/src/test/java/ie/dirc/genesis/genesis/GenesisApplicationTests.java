d7a8b8bd7843b5292ed1037a4a14d980dc0c5c56
Nikita Skobelevs <nikita.skobelevs@gmail.com>
2023-04-05 16:24:27 +0100

    Init genesis service project

---
+++ genesis/src/test/java/ie/dirc/genesis/genesis/GenesisApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.dirc.genesis.genesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
